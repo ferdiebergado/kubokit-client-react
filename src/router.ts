6e73e1aa8229a24626ade1caabb111066c694622
ferdie bergado <ferdiebergado@gmail.com>
2025-08-03 20:55:06 +0800

    refactor: Move all routes as children of root route

--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,9 @@
 import { createBrowserRouter } from 'react-router'
 import App from './App'
-import Home from './Home'
-import { Layout, Signup } from './features/account'
-import NotFound from './NotFound'
 import ErrorFallback from './Error'
+import { Layout, Signup } from './features/account'
+import Home from './Home'
+import NotFound from './NotFound'
 
 export const router = createBrowserRouter([
     {
@@ -15,20 +15,20 @@ export const router = createBrowserRouter([
                 index: true,
                 Component: Home,
             },
-        ],
-    },
-    {
-        path: '/account',
-        Component: Layout,
-        children: [
             {
-                path: 'signup',
-                Component: Signup,
+                path: 'account',
+                Component: Layout,
+                children: [
+                    {
+                        path: 'signup',
+                        Component: Signup,
+                    },
+                ],
+            },
+            {
+                path: '*',
+                Component: NotFound,
             },
         ],
-    },
-    {
-        path: '*',
-        Component: NotFound,
     },
 ])
