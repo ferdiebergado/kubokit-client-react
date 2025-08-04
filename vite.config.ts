94a149f27743c8586b644abc6321b76a597589ac
ferdie bergado <ferdiebergado@gmail.com>
2025-08-04 21:12:16 +0800

    refactor: Replace node:path with URL for path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
 /// <reference types="vitest/config" />
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
     resolve: {
         alias: {
-            '@': path.resolve(__dirname, 'src'),
+            '@': new URL('./src', import.meta.url).pathname,
         },
     },
     plugins: [react()],
